f351c569bc9a6ccb3704e72a78a63743b3ef1ec9
Jasmine <ymj2958@126.com>
2022-06-14 11:06:58 +0800

    Create remote-css.vue
     <remote-css :href="remoteCssHref" />

---
+++ remote-css.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'remote-css',
+  props: {
+    href: {
+      type: String,
+      required: true
+    }
+  },
+  render (h) {
+    return h('link', { attrs: { rel: 'stylesheet', href: this.href } });
+  }
+};
+</script>
