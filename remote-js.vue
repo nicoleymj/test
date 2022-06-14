6ce4e4d4279d22a0cd9d22850f8410e6c4c623ab
Jasmine <ymj2958@126.com>
2022-06-14 11:10:14 +0800

    Create remote-js.vue
    <remote-js :src="remoteJsSrc" />

---
+++ remote-js.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'remote-css',
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+  render (h) {
+    return h("script", { attrs: { type: "text/javascript", src: this.src } });
+  }
+};
+</script>
